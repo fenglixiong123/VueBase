c036a3ec68c4648cbc6162db4da06b70ce046abd
fenglixiong <fenglixiong123@163.com>
2019-04-26 00:21:46 +0800

    初始化提交git项目

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img src="./assets/logo.png">
+<!--    <router-view/>-->
+    <HelloWorld/>
+    <Computer/>
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld";
+import Computer from './components/Computer'
+export default {
+  name: 'App',
+  components: {Computer, HelloWorld}
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Computer.vue
@@ -0,0 +1,28 @@
+<template>
+    <dev class="hello">
+      <hr/>
+      {{msg}}
+      <br/>
+      {{5+2}}
+      <br/>
+      {{5>2?'yes':'no'}}
+      <br/>
+      {{'hello'.split('').reverse().join('')}}
+      <hr/>
+    </dev>
+</template>
+
+<script>
+    export default {
+        name: "Computer",
+        data(){
+            return {
+              msg:'基本指令一'
+            }
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
